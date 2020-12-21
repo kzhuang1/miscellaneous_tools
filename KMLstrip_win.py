e3f3b3aea2af1cd30fc740b034e259622fc5d9a2
kzhuang1 <50504166+kzhuang1@users.noreply.github.com>
2020-12-21 16:54:07 -0700

    Add files via upload

---
+++ KMLstrip_win.py
@@ -0,0 +1,57 @@
+import os           
+  # 
+  # KMLstrip
+  # 
+  # Author: Kai Zhuang
+  # Date:    Dec 2020
+  # 
+  # 
+  # Function   : Code striping 
+  # 
+  # Description: Attempts to strip most of the codes from a KML file leaving mostly text entries
+  #               
+  # 
+  # Parameters : drectory              - monitoring directory for files 
+  # 
+  # Return     : output directory with respective files mostly striped of code
+  # 
+  # Examples of Usage: 
+  # 
+  #    set directory = '/home/kzhuang/Downloads/' with all KML files in the Downloads directory, script will then
+  #    strip all files of code and output to output folder within downloads
+  #    
+  #           
+  # 
+  # 
+
+directory = r"E:\Users\kzhuang\Downloads"
+
+#-----------------------------------should not need to modify past this line ------------------------------------
+outdir = directory+'\output'
+try:
+    os.mkdir(outdir)
+except OSError:
+    print ("Creation of the directory %s failed" % outdir)
+else:
+    print ("Successfully created the directory %s " % outdir)
+    
+def main():
+    for entry in os.scandir(directory):
+        if (entry.path.endswith(".kml")) and entry.is_file():
+            print(entry.path)
+            filename=entry.path.lstrip(directory)
+            filename=filename.rstrip(".kml")
+            print(outdir)
+            with open(entry.path, 'r') as infile, open(outdir +'\\'+ filename +'.txt', 'w') as outfile:
+                for line in infile:
+                    if "<description>" in line:
+                        line=line.lstrip()
+                        line=line.rstrip()
+                        line=line.lstrip("<description>![CDATA[")
+                        line=line.rstrip("</div>]]</description>")
+                        #print(line)
+                        outfile.write(line+'\n')
+            infile.close()
+            outfile.close()
+            
+main()
